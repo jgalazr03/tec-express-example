707f5a91c601cafc4704b6812b07e19c3893c6e8
Jesus Galaz <jesusgalazr@icloud.com>
2025-03-31 12:25:33 -0600

    Fix lint errors

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const customerRoutes = require("./src/routes/customer");
-const errorMiddleware = require("./src/middleware/error");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const customerRoutes = require('./src/routes/customer');
+const errorMiddleware = require('./src/middleware/error');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.use("/api/customers", customerRoutes);
+app.use('/api/customers', customerRoutes);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
